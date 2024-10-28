0bf235a29adfce8c9ce6542ee49699d07b46a5bc
esdawood <esoodawood186@gmail.com>
2024-10-28 21:57:37 +0300

    - add cv icon link - add download cv file

--- lib/core/helper/app_functions.dart
+++ lib/core/helper/app_functions.dart
@@ -1,3 +1,10 @@
+import 'package:dio/dio.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:universal_html/html.dart' as html;
+import 'package:path_provider/path_provider.dart';
+import 'package:portfolio/core/widgets/custom_snack_bar.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class AppFunctions{
@@ -11,4 +18,57 @@ class AppFunctions{
     }
   }
 
+  static Future<bool> downloadFile(BuildContext context) async {
+    String pdfUrl =
+        "https://drive.google.com/uc?export=download&id=1OhJrHrej5Pt4qN5Nx4EAhWmZ3s5C6_Mu"; // Direct download link
+
+    if (kIsWeb) {
+      // Web: Open the file in a new tab for download
+      html.AnchorElement(href: pdfUrl)
+        ..setAttribute('download', 'cv.pdf')
+        ..click();
+      showSnackBar(context: context, message: "Downloading....");
+      return true;
+    } else {
+      try {
+        Dio dioDownload = Dio();
+
+        // Select the download directory for mobile/desktop platforms
+        String? outputFile = await FilePicker.platform.saveFile(
+          initialDirectory: await getDownloadsDirectory().then((value) => value?.path),
+          dialogTitle: 'Choose location to save',
+          allowedExtensions: ['pdf'],
+          type: FileType.custom,
+          fileName: 'cv.pdf',
+        );
+
+        if (outputFile != null) {
+          // Ensure .pdf extension
+          if (!outputFile.endsWith('.pdf')) {
+            outputFile += '.pdf';
+          }
+
+          final response = await dioDownload.download(pdfUrl, outputFile,
+              onReceiveProgress: (receivedBytes, totalBytes) {
+                int progress = ((receivedBytes / totalBytes) * 100).floor();
+                showSnackBar(context: context, message: "Downloading.... $progress%");
+
+              });
+
+          if (response.statusCode == 200) {
+           if(context.mounted) showSnackBar(context: context, message: "Download completed%");
+
+            return true;
+          } else {
+            return false;
+          }
+        } else {
+          return false;
+        }
+      } catch (e) {
+        return false;
+      }
+    }
+  }
 }
+
