4078230f9c60a2386dd2e850cb20babdf26de6b4
esdawood <esoodawood186@gmail.com>
2025-02-01 21:15:04 +0200

    - edit in  social icon - edit  hover in download cv file

--- lib/features/home/screen/widgets/download_cv_file.dart
+++ lib/features/home/screen/widgets/download_cv_file.dart
@@ -5,25 +5,45 @@ import 'package:portfolio/core/theme/app_color.dart';
 import 'package:portfolio/core/theme/app_text_styles.dart';
 import 'package:portfolio/core/theme/font_family_helper.dart';
 
-class DownloadCvFile extends StatelessWidget {
+class DownloadCvFile extends StatefulWidget {
   const DownloadCvFile({super.key});
 
   @override
+  State<DownloadCvFile> createState() => _DownloadCvFileState();
+}
+
+class _DownloadCvFileState extends State<DownloadCvFile> {
+  bool isHover = false;
+
+  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: (){
+    return InkWell(
+      onTap: () {
         AppFunctions.downloadFile(context);
       },
+      onHover: (val) {
+        setState(() {
+          isHover =  val;
+        });
+      },
+      hoverColor: Colors.transparent,
       child: Container(
-        padding:  EdgeInsets.symmetric(horizontal: 12.w, vertical: 8.h),
+        padding: EdgeInsets.symmetric(horizontal: 12.w, vertical: 8.h),
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(12).r,
-            border: Border.all(color: AppColor.brandeisBlue, width: 2)),
+          borderRadius: BorderRadius.circular(12).r,
+          color: isHover ? AppColor.brandeisBlue   : AppColor.white ,
+          border:
+          Border.all(
+            color: isHover ? Colors.transparent : AppColor.brandeisBlue,
+            width: 2,
+          ),
+        ),
         child: Center(
           child: Text(
             'Download CV file',
             style: AppTextStyles.font22BlackBold(context).copyWith(
               fontFamily: FontFamilyHelper.robotoFont,
+              color: isHover ? AppColor.white  : AppColor.black
             ),
           ),
         ),
